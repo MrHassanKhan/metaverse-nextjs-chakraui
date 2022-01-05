2258b03002737e64fae613e9b4ece453a9c52de0
Muhammad Hassan <muhammadhsn09@gmail.com>
2022-01-05 11:23:47 +0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MoralisProvider } from "react-moralis"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={process.env.NEXTAPPID || ''}
+      serverUrl={process.env.NEXTSERVERURL || ''}>
+      <ChakraProvider>
+            <Component {...pageProps} />
+      </ChakraProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
